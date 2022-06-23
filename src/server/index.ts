38f5b8499f8f3677777ccf404c6960b828b1af99
Alcione <alcionefribeiro@gmail.com>
2022-06-23 22:17:56 +0200

    Install sqlite3 lib

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,12 @@
-export default async function main() {
-  // something here
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data/instagram_influencers.sqlite3');
+
+async function main() {
+    db.serialize(() => {
+        db.get("select * from influencers LIMIT 1", (err: Error, row: any) => {
+            console.log(row);
+        });
+    });
 }
+
+main().then();
